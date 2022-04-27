82b25a678829079377cd696f35e4a664e3e88557
Aji Siwi <182410101122@students.unej.ac.id>
2022-04-28 01:02:37 +0700

    update frontend

--- resources/views/public/beranda/index.blade.php
+++ resources/views/public/beranda/index.blade.php
@@ -1,5 +1,165 @@
 @extends('public.layout.app')
 
+@section('style')
+    <link rel="stylesheet" href="{{ asset('css/beranda.css') }}">
+@endsection
+
 @section('content')
-    <p>test</p>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('storage/images/background.png') }}" class="d-block w-100" alt="carousel-1">
+            </div>
+        </div>
+    </div>
+    <div class="game-verse d-flex flex-warp align-items-center">
+        <div class="px-3 mx-auto">
+            <img class="img-fluid" src="{{ asset('storage/images/game-verse/candy.png') }}" alt="candy">
+        </div>
+        <div class="px-3 mx-auto">
+            <img class="img-fluid" src="{{ asset('storage/images/game-verse/lemon-sky.png') }}" alt="lemon-sky">
+        </div>
+        <div class="px-3 mx-auto">
+            <img class="img-fluid" src="{{ asset('storage/images/game-verse/jericverse.png') }}" alt="jericverse">
+        </div>
+        <div class="px-3 mx-auto">
+            <img class="img-fluid" src="{{ asset('storage/images/game-verse/gameconomy.png') }}" alt="gameconomy">
+        </div>
+    </div>
+
+    <div class="container-fluid frame-3 py-5">
+        <div class="row">
+            <div class="col-12 d-flex">
+                <img class="logo mx-auto" src="{{ asset('storage/images/navbar-logo.png') }}" alt="logo">
+            </div>
+            <div class="row play-background">
+                <div class="frame-item col-lg-4 col-md-12 col-12 text-white">
+                    <h5 class="fw-bolder mb-4">INTO THE METAVERSE</h5>
+                    <p class="mt-4">Earn, trade, stake, farm and swap your game assets the way you want. Welcome
+                        to a
+                        metaverse that lets you truly decide how you want to play.</p>
+                </div>
+                <div class="col-lg-4 col-md-12 col-12 d-flex">
+                    <img class="img-play img-fluid mx-auto my-auto" src="{{ asset('storage/images/play-button.png') }}" alt="play-btn">
+                </div>
+                <div class="frame-item col-lg-4 col-md-12 col-12 text-white">
+                    <p>This is for all the gamers out there no matter who you are, whether you're a newbie or a whale. It's
+                        time
+                        to game on your own terms. Welcome to Joy Games - Metaverse gaming by true game makers.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid px-5 frame-4 mt-5">
+        <div class="px-4 mb-5">
+            <h2 class="text-center mb-5">Upcoming Games</h2>
+            <div class="d-flex mt-5">
+                <p class="text-center text-white mx-auto">Joy Games aims to not just create a platform but also will play an
+                    active role in the development and curation of
+                    gaming content on the platform, the upkeep of the codebase, and the development of new exciting ways to
+                    implement gamified finance into existing or new games, via a mixture of of inhouse development or
+                    co-development
+                    with our strategic partners, who are experienced and successful game development studios.</p>
+            </div>
+        </div>
+        <div class="d-flex flex-wrap justify-content-evenly mt-5">
+            <button class="btn cs-btn-lg mx-3 my-3">Adventure</button>
+            <button class="btn cs-btn-lg mx-3 my-3">Arcade</button>
+            <button class="btn cs-btn-lg mx-3 my-3">Race</button>
+            <button class="btn cs-btn-lg mx-3 my-3">Open World</button>
+            <button class="btn cs-btn-lg mx-3 my-3">RPG</button>
+            <button class="btn cs-btn-lg mx-3 my-3">MMORPG</button>
+            <button class="btn cs-btn-lg mx-3 my-3">FPS</button>
+            <button class="btn cs-btn-lg mx-3 my-3">MULTIPLAYER</button>
+        </div>
+    </div>
+
+    <div class="container-fluid px-5 frame-5 mt-5">
+        <div class="d-flex flex-wrap justify-content-evenly">
+            <div class="game-card my-3 position-relative">
+                <img class="img-fluid" src="{{ asset('storage/images/game-card/image-57.png') }}"
+                    alt="game-image">
+                <div class="descriptions p-3 position-absolute">
+                    <p class="text-white">Enter the world of Masketeers where brave heroes empowered by mysterious masks take a stand against
+                        the inner demons of society. Soon you will be able to adorn yourself with exclusive NFT Costumes
+                        that can empower your Masketeers as they unleash their attacks on the manifestations of negativity.
+                    </p>
+                </div>
+                <div class="title p-3 w-100 position-absolute">
+                    <h4 class="fw-bold text-white">Claw Stars</h4>
+                </div>
+            </div>
+
+            <div class="game-card my-3 position-relative">
+                <img class="img-fluid" src="{{ asset('storage/images/game-card/image-58.png') }}"
+                    alt="game-image">
+                <div class="descriptions p-3 position-absolute">
+                    <p class="text-white">Enter the world of Masketeers where brave heroes empowered by mysterious masks take a stand against
+                        the inner demons of society. Soon you will be able to adorn yourself with exclusive NFT Costumes
+                        that can empower your Masketeers as they unleash their attacks on the manifestations of negativity.
+                    </p>
+                </div>
+                <div class="title p-3 w-100 position-absolute">
+                    <h4 class="fw-bold text-white">The Last</h4>
+                </div>
+            </div>
+
+            <div class="game-card my-3 position-relative">
+                <img class="img-fluid" src="{{ asset('storage/images/game-card/image-59.png') }}"
+                    alt="game-image">
+                <div class="descriptions p-3 position-absolute">
+                    <p class="text-white">Enter the world of Masketeers where brave heroes empowered by mysterious masks take a stand against
+                        the inner demons of society. Soon you will be able to adorn yourself with exclusive NFT Costumes
+                        that can empower your Masketeers as they unleash their attacks on the manifestations of negativity.
+                    </p>
+                </div>
+                <div class="title p-3 w-100 position-absolute">
+                    <h4 class="fw-bold text-white">Moon Land</h4>
+                </div>
+            </div>
+
+            <div class="game-card my-3 position-relative">
+                <img class="img-fluid" src="{{ asset('storage/images/game-card/image-57.png') }}"
+                    alt="game-image">
+                <div class="descriptions p-3 position-absolute">
+                    <p class="text-white">Enter the world of Masketeers where brave heroes empowered by mysterious masks take a stand against
+                        the inner demons of society. Soon you will be able to adorn yourself with exclusive NFT Costumes
+                        that can empower your Masketeers as they unleash their attacks on the manifestations of negativity.
+                    </p>
+                </div>
+                <div class="title p-3 w-100 position-absolute">
+                    <h4 class="fw-bold text-white">Claw Stars</h4>
+                </div>
+            </div>
+
+            <div class="game-card my-3 position-relative">
+                <img class="img-fluid" src="{{ asset('storage/images/game-card/image-58.png') }}"
+                    alt="game-image">
+                <div class="descriptions p-3 position-absolute">
+                    <p class="text-white">Enter the world of Masketeers where brave heroes empowered by mysterious masks take a stand against
+                        the inner demons of society. Soon you will be able to adorn yourself with exclusive NFT Costumes
+                        that can empower your Masketeers as they unleash their attacks on the manifestations of negativity.
+                    </p>
+                </div>
+                <div class="title p-3 w-100 position-absolute">
+                    <h4 class="fw-bold text-white">The Last</h4>
+                </div>
+            </div>
+
+            <div class="game-card my-3 position-relative">
+                <img class="img-fluid" src="{{ asset('storage/images/game-card/image-59.png') }}"
+                    alt="game-image">
+                <div class="descriptions p-3 position-absolute">
+                    <p class="text-white">Enter the world of Masketeers where brave heroes empowered by mysterious masks take a stand against
+                        the inner demons of society. Soon you will be able to adorn yourself with exclusive NFT Costumes
+                        that can empower your Masketeers as they unleash their attacks on the manifestations of negativity.
+                    </p>
+                </div>
+                <div class="title p-3 w-100 position-absolute">
+                    <h4 class="fw-bold text-white">Moon Land</h4>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
---
+++ resources/views/public/layout/navbar.blade.php
@@ -0,0 +1,40 @@
+<nav class="navbar sticky-top navbar-expand-lg  w-100">
+    <div class="container-fluid px-5 cs-navbar-color py-3">
+        <a class="navbar-brand" href="#">
+            <img class="logo" src="{{ asset('storage/images/navbar-logo.png') }}" alt="logo">
+        </a>
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+                <i class="fas fa-bars text-white"></i>
+            </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a href="#">ABOUT</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">STORE</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">GAMEBOX</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">TOKEN</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">STAKING</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">DOC</a>
+                </li>
+            </ul>
+            <div class="login-navbar ms-auto">
+                <a href="#" class="nav-item">REGISTER</a>
+                <button class="nav-item btn-login">LOGIN</button>
+            </div>
+        </div>
+    </div>
+</nav>
